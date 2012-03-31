d9cff5459d6f33eb7c96df2b5c5e87e0161ad5c6
Matteo Mecucci <djinn.rtl@gmail.com>
2012-03-31 13:48:14 +0200

    Adding downloading of files as ogg with custom quality.

--- beetsplug/web/__init__.py
+++ beetsplug/web/__init__.py
@@ -83,7 +83,6 @@ def item_file(item_id):
 def item_ogg(item_id, ogg_q):
     from subprocess import Popen, PIPE
     import mimetypes;
-<<<<<<< HEAD
     item = g.lib.get_item(item_id)
     filename = os.path.split(item.path)[1]
     filename = os.path.splitext(filename)[0] + '.ogg'
@@ -122,8 +121,6 @@ def item_stream(item_id):
 @app.route('/item/<int:item_id>/ogg_q<int:ogg_q>')
 def item_ogg(item_id, ogg_q):
     from subprocess import Popen, PIPE
-=======
->>>>>>> Made some adjustments to the function that encodes to ogg in the web plugin.
     item = g.lib.get_item(item_id)
     filename = os.path.split(item.path)[1]
     filename = os.path.splitext(filename)[0] + '.ogg'
@@ -158,6 +155,31 @@ def item_ogg(item_id, ogg_q):
 @app.route('/item/<int:item_id>/stream')
 def item_stream(item_id):
   return app.config['stream_func'](item_id)
+
+@app.route('/item/<int:item_id>/ogg_q<int:ogg_q>')
+def item_ogg(item_id, ogg_q):
+    from subprocess import Popen, PIPE
+    item = g.lib.get_item(item_id)
+
+    if item.path.endswith('.mp3'):
+      decoded_fp = Popen(
+          ["mpg123", "-q", "-w", "/dev/stdout", item.path],
+          stdout=PIPE)
+      ogg_fp = Popen(
+          ["oggenc", "-q", str(ogg_q), "-Q", "-"],
+          stdin=decoded_fp.stdout,
+          stdout=PIPE);
+      decoded_fp.stdout.close()
+    else:
+      ogg_fp = Popen(
+          ["oggenc", "-q",  str(ogg_q),"-Q", "-o", "/dev/stdout", item.path],
+          stdout=PIPE);
+
+    headers = Headers()
+    headers.add('Content-Type', 'audio/ogg')
+    filename = item.path.rpartition('/')[2].rsplit('.',1)[0] + '.ogg'
+    headers.add('Content-Disposition', 'attachment', filename=filename)
+    return Response(ogg_fp.stdout, headers=headers)
 
 @app.route('/item/query/<path:query>')
 def item_query(query):
