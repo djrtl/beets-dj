1d0a56a55cffb8f5c05f80ae56db5936a0f7a1fb
Matteo Mecucci <djinn.rtl@gmail.com>
2012-03-31 13:48:14 +0200

    Adding downloading of files as ogg with custom quality.

--- beetsplug/web/__init__.py
+++ beetsplug/web/__init__.py
@@ -18,7 +18,8 @@ from beets import ui
 from beets.importer import _reopen_lib
 import beets.library
 import flask
-from flask import g
+from flask import g, Response
+from werkzeug.datastructures import Headers
 
 DEFAULT_HOST = ''
 DEFAULT_PORT = 8337
@@ -67,6 +68,31 @@ def all_items():
 def item_file(item_id):
     item = g.lib.get_item(item_id)
     return flask.send_file(item.path)
+
+@app.route('/item/<int:item_id>/ogg_q<int:ogg_q>')
+def item_ogg(item_id, ogg_q):
+    from subprocess import Popen, PIPE
+    item = g.lib.get_item(item_id)
+
+    if item.path.endswith('.mp3'):
+      decoded_fp = Popen(
+          ["mpg123", "-q", "-w", "/dev/stdout", item.path],
+          stdout=PIPE)
+      ogg_fp = Popen(
+          ["oggenc", "-q", str(ogg_q), "-Q", "-"],
+          stdin=decoded_fp.stdout,
+          stdout=PIPE);
+      decoded_fp.stdout.close()
+    else:
+      ogg_fp = Popen(
+          ["oggenc", "-q",  str(ogg_q),"-Q", "-o", "/dev/stdout", item.path],
+          stdout=PIPE);
+
+    headers = Headers()
+    headers.add('Content-Type', 'audio/ogg')
+    filename = item.path.rpartition('/')[2].rsplit('.',1)[0] + '.ogg'
+    headers.add('Content-Disposition', 'attachment', filename=filename)
+    return Response(ogg_fp.stdout, headers=headers)
 
 @app.route('/item/query/<path:query>')
 def item_query(query):
