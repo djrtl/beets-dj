98ece23ab9fef387fe1521db75dc8b2f8751fc7f
Matteo Mecucci <djinn.rtl@gmail.com>
2012-04-02 00:56:44 +0200

    Made some adjustments to the function that encodes to ogg in the web plugin.

--- beetsplug/web/__init__.py
+++ beetsplug/web/__init__.py
@@ -18,9 +18,15 @@ from beets import ui
 from beets.importer import _reopen_lib
 import beets.library
 import flask
-from flask import g, Response
+from flask import g, Response, request
 from werkzeug.datastructures import Headers
 import re
+# this was moved in 0.7
+try:
+    from werkzeug.wsgi import wrap_file
+except ImportError:
+    from werkzeug.utils import wrap_file
+
 
 DEFAULT_HOST = ''
 DEFAULT_PORT = 8337
@@ -74,27 +80,37 @@ def item_file(item_id):
 @app.route('/item/<int:item_id>/ogg_q<int:ogg_q>')
 def item_ogg(item_id, ogg_q):
     from subprocess import Popen, PIPE
+    import mimetypes;
     item = g.lib.get_item(item_id)
-
-    if item.path.endswith('.mp3'):
-      decoded_fp = Popen(
-          ["mpg123", "-q", "-w", "/dev/stdout", item.path],
-          stdout=PIPE)
-      ogg_fp = Popen(
-          ["oggenc", "-q", str(ogg_q), "-Q", "-"],
-          stdin=decoded_fp.stdout,
-          stdout=PIPE);
-      decoded_fp.stdout.close()
-    else:
-      ogg_fp = Popen(
-          ["oggenc", "-q",  str(ogg_q),"-Q", "-o", "/dev/stdout", item.path],
-          stdout=PIPE);
+    filename = os.path.split(item.path)[1]
+    filename = os.path.splitext(filename)[0] + '.ogg'
 
     headers = Headers()
     headers.add('Content-Type', 'audio/ogg')
-    filename = item.path.rpartition('/')[2].rsplit('.',1)[0] + '.ogg'
     headers.add('Content-Disposition', 'attachment', filename=filename)
-    return Response(ogg_fp.stdout, headers=headers)
+
+    if mimetypes.guess_type(item.path)[0] == 'audio/mpeg':
+        decoded_fp = Popen(
+            ["mpg123", "-q", "-w", "/dev/stdout", item.path],
+            stdout=PIPE)
+        ogg_fp = Popen(
+            ["oggenc", "-q", str(ogg_q), "-Q", "-"],
+            stdin=decoded_fp.stdout,
+            stdout=PIPE);
+        decoded_fp.stdout.close()
+    else:
+        ogg_fp = Popen(
+            ["oggenc", "-q",  str(ogg_q),"-Q", "-o", "/dev/stdout", item.path],
+            stdout=PIPE);
+
+    res = Response(
+        #wrap_file(request.environ, ogg_fp.stdout),
+        ogg_fp.stdout,
+        headers=headers,
+        direct_passthrough=True)
+    res.implicit_sequence_conversion = False
+
+    return res
 
 @app.route('/item/<int:item_id>/stream')
 def item_stream(item_id):
