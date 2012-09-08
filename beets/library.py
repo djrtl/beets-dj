797e4bc2a785d1513232839c8597c7a717d77968
Adrian Sampson <adrian@radbox.org>
2012-09-08 16:24:39 -0700

    fix match() on RegexpQuery for non-string values

--- beets/library.py
+++ beets/library.py
@@ -466,6 +466,10 @@ class RegexpQuery(FieldQuery):
 
     def match(self, item):
         value = getattr(item, self.field) or ''
+        if value is None:
+            value = u''
+        elif not isinstance(value, basestring):
+            value = unicode(value)
         return self.regexp.search(value) is not None
 
 class BooleanQuery(MatchQuery):
