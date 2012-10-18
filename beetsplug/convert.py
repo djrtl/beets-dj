d2b331b5740d6069474fa54ef2a65504f399c76c
Jakob Schnitzer <mail@jakobschnitzer.de>
2012-10-18 18:35:25 +0200

    convert: fix album art embedding

--- beetsplug/convert.py
+++ beetsplug/convert.py
@@ -139,7 +139,7 @@ def convert_item(lib, dest_dir):
 
         artpath = lib.get_album(item).artpath
         if artpath and conf['embed']:
-            _embed(artpath, [dest_item])
+            _embed(artpath, [item])
 
 
 def convert_func(lib, config, opts, args):
